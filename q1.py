10758bc5e8ef1ab40fdbab5464d1f31254e468c7
Ryal rafter amanna <103073325+Ryal333@users.noreply.github.com>
2022-05-16 20:45:36 +0530

    Quiz-1 Driving eligibility

---
+++ q1.py
@@ -0,0 +1,8 @@
+print("enter your age: ")
+age=int(input())
+if age>18:
+    print("you are eligible to drive")
+elif age<18:
+    print("you are not eligible to drive")
+else:
+    print("you need to take a test")
