3210fd3f7b798c55cb1ca9be6b7288277d2930f4
Ryal rafter amanna <103073325+Ryal333@users.noreply.github.com>
2022-05-16 20:37:08 +0530

    Ex1 Dictionary

---
+++ ex1.py
@@ -0,0 +1,5 @@
+print("Student grading system")
+D={34:"fail",35:"pass",50:"second class",60:"first class",85:"distinction"}
+print("enter the value: ")
+I=input()
+print("The result is "+ D[int(I)])
