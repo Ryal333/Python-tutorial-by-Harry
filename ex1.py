de39f0da1623625c039d3cbb17287b0eb72415f2
Ryal rafter amanna <103073325+Ryal333@users.noreply.github.com>
2022-05-16 20:31:57 +0530

    Delete ex1.py

--- ex1.py
+++
@@ -1,5 +0,0 @@
-print("Student grading system")
-D={34:"fail",35:"pass",50:"second class",60:"first class",85:"distinction"}
-print("enter the value: ")
-I=input()
-print("The result is "+ D[int(I)])
